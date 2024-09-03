0743a82ae5f6ed74dfa8b26031fbf45ead65570b
Kenneth P. Hough <kenneth@egtech.us>
2024-09-03 20:40:35 +0900

    Update path for stream

--- file_serving/urls.py
+++ file_serving/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('download/<str:filename>/', views.serve_file, name='serve_file'),
-    path('download/stream/<str:filename>/', views.serve_file_stream, name='serve_file_stream'),
+    path('stream/<str:filename>/', views.serve_file_stream, name='serve_file_stream'),
 ]
 
